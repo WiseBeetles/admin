56c2722e9d855c4654e4c40202881f23d4b3e8c6
AlexWebWP <alexzhukoveysk123@gmail.com>
2024-06-09 13:48:07 +0400

    move errors in folder

--- src/Framework/ErrorHandler.php
+++ src/Framework/ErrorHandler.php
@@ -34,7 +34,9 @@ class ErrorHandler
             ini_set("display_errors", "0");
             ini_set("log_errors", "1");
 
-            require dirname(__DIR__, 2) . VIEWS_PATH . $template;
+//            echo dirname(__DIR__, 2) . VIEWS_PATH . 'errors/' . $template;
+
+            require dirname(__DIR__, 2) . VIEWS_PATH . 'errors/' . $template;
         }
 
         throw $exception;
